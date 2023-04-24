8a6b389323bc0d79d893b088ba06be5f061772f5
kubiksonikdev <122747588+kubiksonikdev@users.noreply.github.com>
2023-04-24 15:23:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
-function openCase() {
-  const items = [
-    'P2000 | Amber Fade',
-    'Glock-18 | Candy Apple',
-    'AK-47 | Blue Laminate',
-    'AWP | Pit Viper',
-    'M4A1-S | Bright Water',
-    'Desert Eagle | Crimson Web',
-    'USP-S | Torque',
-    'MAC-10 | Candy Apple',
-    'UMP-45 | Corporal',
-    'P90 | Death by Kitty',
-    'Sawed-Off | The Kraken',
-    'M249 | System Lock',
-    'Galil AR | Chatterbox',
-    'Nova | Antique',
-    'XM1014 | Blue Steel',
-    'Negev | Terrain',
-    'SSG 08 | Blue Spruce',
-    'Dual Berettas | Stained',
-    'Tec-9 | Brass',
-    'P250 | Steel Disruption',
-    'Five-SeveN | Urban Hazard',
-    'CZ75-Auto | The Fuschia Is Now',
-    'MP9 | Deadly Poison',
-    'MP7 | Army Recon',
-    'SG 553 | Ultraviolet',
-    'AUG | Wings',
-    'FAMAS | Spitfire',
-    'SCAR-20 | Contractor',
-    'G3SG1 | Desert Storm',
-    'PP-Bizon | Osiris'
-  ];
-  
-  const randomItem = items[Math.floor(Math.random() * items.length)];
-  
-  document.getElementById('resultText').textContent = `You got: ${randomItem}!`;
-}
+
+JavaScript:
+
+```javascript
+// Code for opening/closing cases
+const cases = document.querySelectorAll(".case");
+const modal = document.querySelector(".modal");
+const modalContent = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");
+
+cases.forEach((el) => {
+  el.addEventListener("click", () => {
+    modalContent.innerHTML = `
+      <h2 class="modal-title">${el.dataset.title}</h2>
+      <img src="${el.dataset.img}" alt="${el.dataset.title}" class="modal-img">
+      <div class="modal-details">
+        <p class="modal-price">${el.dataset.price}</p>
+        <button class="modal-buy cta">Buy Now</button>
+      </div>
+    `;
+    modal.classList.add("modal-active");
+  });
+});
+
+modalClose.addEventListener("click", () => {
+  modal.classList.remove("modal-active");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.classList.remove("modal-active");
+  }
+});
